9f35832647d81d5aa7c7a06fb02a97a36fa25fbc
varu3dbox <subplus.1027magi+tech@gmail.com>
2020-09-10 23:16:08 +0900

    1512. Number of Good Pairs

---
+++ leetCode/numIdenticalPairs.ts
@@ -0,0 +1,12 @@
+function numIdenticalPairs(nums: number[]): number {
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            // console.log(`${nums[i]}, ${nums[j]}`)
+            if (nums[i] === nums[j]) {
+                count++
+            }
+        }
+    }
+    return count
+}
